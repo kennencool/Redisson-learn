62cf4fe119391aecec2869164c19dfe12ef53718
kennencool <3304192108@qq.com>
2021-03-22 14:38:54 +0800

    新增两种并发控制： setnx配合锁变量、redisson RLock锁

--- redis_high_concurrency_test/src/main/java/com/kennen/redis_high_concurrency_test/RedisHighConcurrencyTestApplication.java
+++ redis_high_concurrency_test/src/main/java/com/kennen/redis_high_concurrency_test/RedisHighConcurrencyTestApplication.java
@@ -1,7 +1,10 @@
 package com.kennen.redis_high_concurrency_test;
 
+import org.redisson.Redisson;
+import org.redisson.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisHighConcurrencyTestApplication {
@@ -10,4 +13,11 @@ public class RedisHighConcurrencyTestApplication {
 		SpringApplication.run(RedisHighConcurrencyTestApplication.class, args);
 	}
 
+	@Bean
+	public Redisson redisson(){
+		// 单机模式
+		Config config = new Config();
+		config.useSingleServer().setAddress("redis://localhost:6380").setDatabase(4);
+		return (Redisson)Redisson.create(config);
+	}
 }
--- redis_high_concurrency_test/src/main/java/com/kennen/redis_high_concurrency_test/controller/IndexController.java
+++ redis_high_concurrency_test/src/main/java/com/kennen/redis_high_concurrency_test/controller/IndexController.java
@@ -1,13 +1,14 @@
 package com.kennen.redis_high_concurrency_test.controller;
 
 import org.redisson.Redisson;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.redisson.api.RLock;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: hejiyuan
@@ -16,8 +17,14 @@ import javax.annotation.Resource;
  */
 @RestController
 public class IndexController {
+    @Resource private Redisson redisson;
     @Resource private RedisTemplate redisTemplate;
-    
+
+    /**
+     * 使用 synchronized 关键字实现并发控制
+     * 缺点：当有请求访问开始执行时，其他请求被阻塞，接口网络吞吐率很低
+     * @return
+     */
     @RequestMapping("/deduct_stock")
     public String deductStock(){
         // 使用Jmeter高并发测试，发现打印结果都为49，因为所有请求在并发访问时，并发读取的库存都是50
@@ -41,6 +48,61 @@ public class IndexController {
                 System.out.println("扣减失败，库存不足");
             }
         }
+        return "end";
+    }
+
+    /**
+     * 通过 setnx(setIfAbsent) 设置锁变量来实现并发控制
+     * 缺点：当有请求在执行时，其他请求不会被阻塞，而是直接返回操作失败。
+     * @return
+     */
+    @RequestMapping("/deduct_stock1")
+    public String deductStock1(){
+        // 这里的id是为了防止创建的锁名和其他线程的锁一样，导致被其他线程解开
+        String lockKey = "lockKey";
+        String id = UUID.randomUUID().toString();
+        // 设置超时时间是防止因为服务器挂掉或出异常导致的锁没有被清除
+        Boolean lock = redisTemplate.opsForValue().setIfAbsent(lockKey,id,10,TimeUnit.SECONDS);
+        if(!lock){
+            return "error_other_using";
+        }
+        int stock = Integer.parseInt((String)redisTemplate.opsForValue().get("stock"));
+        if(stock > 0){
+            int newStock = stock - 1;
+            redisTemplate.opsForValue().set("stock",newStock+"");
+            System.out.println("扣减成功，剩余库存：" + newStock);
+        }else{
+            System.out.println("扣减失败，库存不足");
+        }
+        if(id.equals(redisTemplate.opsForValue().get(lockKey))){
+            redisTemplate.delete(lockKey);
+        }
+        
+        return "end";
+    }
+
+    /**
+     * 通过 redisson 提供的 RLock 锁来实现并发控制
+     * 缺点：
+     * @return
+     */
+    @RequestMapping("/deduct_stock2")
+    public String deductStock2(){
+        String lockKey = "lockKey";
+        RLock redissonLock = redisson.getLock(lockKey);
+        // 加锁
+        redissonLock.lock();
+        int stock = Integer.parseInt((String)redisTemplate.opsForValue().get("stock"));
+        if(stock > 0){
+            int newStock = stock - 1;
+            redisTemplate.opsForValue().set("stock",newStock+"");
+            System.out.println("扣减成功，剩余库存：" + newStock);
+        }else{
+            System.out.println("扣减失败，库存不足");
+        }
+        // 释放锁
+        redissonLock.unlock();
+        
         return "end";
     }
 }
